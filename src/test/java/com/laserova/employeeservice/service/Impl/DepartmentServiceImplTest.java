5f389577e058ba4dc065bbf2c1996986953f7882
Larisa <laserova@yandex.ru>
2023-08-18 00:20:30 +0500

    with tests for EmployeeService and DepartmentService

--- src/test/java/com/laserova/employeeservice/service/Impl/DepartmentServiceImplTest.java
+++ src/test/java/com/laserova/employeeservice/service/Impl/DepartmentServiceImplTest.java
@@ -3,6 +3,7 @@ package com.laserova.employeeservice.service.Impl;
 import com.laserova.employeeservice.dto.Employee;
 import com.laserova.employeeservice.exceptions.EmployeeNotFoundException;
 import com.laserova.employeeservice.service.EmployeeService;
+import net.bytebuddy.description.type.TypeList;
 import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,10 +13,10 @@ import org.mockito.Mock;
 
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,7 +32,7 @@ class DepartmentServiceImplTest {
     Employee alex = new Employee("Alex", "Ivanov", 1, 40000);
     Employee ivan = new Employee("Ivan", "Ivanov", 1, 50000);
     Employee irina = new Employee("Irina", "Glebova", 1, 45000);
-    Employee mark = new Employee("Mark", "Nikolaev", 4, 60000);
+    Employee mark = new Employee("Mark", "Nikolaev", 3, 60000);
 
     Collection<Employee> employees;
 
@@ -50,7 +51,6 @@ class DepartmentServiceImplTest {
     @Test
     void findMaxSalaryEmployee_employeeNotFound_thrownEmployeeNotFoundException() {
         when(employeeService.findAllEmployees()).thenReturn(Collections.emptyList());
-
         assertThrows(EmployeeNotFoundException.class, () -> underTest.findMaxSalaryEmployee(1));
     }
 
@@ -64,31 +64,30 @@ class DepartmentServiceImplTest {
     @Test
     void findMinSalaryEmployee_employeeNotFound_thrownEmployeeNotFoundException() {
         when(employeeService.findAllEmployees()).thenReturn(Collections.emptyList());
-
         assertThrows(EmployeeNotFoundException.class, () -> underTest.findMinSalaryEmployee(1));
     }
 
     @Test
     void getAll_HasNoEmployeeInDepartment_returnEmptyCollections() {
         when(employeeService.findAllEmployees()).thenReturn(employees);
+        Collection<Employee> result = underTest.getAll(2);
+        assertIterableEquals(Collections.emptySet(), result);
     }
 
     @Test
     void getAll_HaveEmployeeInDepartment_returnEmployeeCollectionsInDepartment() {
         when(employeeService.findAllEmployees()).thenReturn(employees);
-        Collection <Employee> result = underTest.getAll(1);
-
-        assertIterableEquals(Arrays.asList(new Employee[]{alex,ivan,irina,}), result);
-    }
-
-    @Test
-    void getAllGroupingByDepartment_HasNoEmployeeInDepartment_returnEmptyCollectionsInDepartment() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
+        Collection<Employee> result = underTest.getAll(1);
+        assertIterableEquals(Arrays.asList(new Employee[]{alex, ivan, irina,}), result);
     }
 
     @Test
     void getAllGroupingByDepartment_HaveEmployeeInDepartment_returnEmployeeCollectionsGroupingByDepartment() {
         when(employeeService.findAllEmployees()).thenReturn(employees);
+        Map<Integer, List<Employee>> result = underTest.getAllGroupingByDepartment();
+        Map<Integer, List<Employee>> expect = employeeService.findAllEmployees().stream()
+                .collect(Collectors.groupingBy(e ->e.getDepartment()));
+        assertEquals(expect, result);
     }
 
 }
